6216384b748621cce2e8bf01e228d7a360d96913
William Dyess <william@deckcraft.com>
2026-01-17 00:37:36 -0500

    Remove Our Ecosystem section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import LogoCarousel from "@/components/LogoCarousel";
 import Advisors from "@/components/Advisors";
 import FeaturedSection from "@/components/FeaturedSection";
 import AboutPreview from "@/components/AboutPreview";
-import AnimatedCircles from "@/components/AnimatedCircles";
 import LinksSection from "@/components/LinksSection";
 import Footer from "@/components/Footer";
 
@@ -18,7 +17,6 @@ export default function Home() {
         <Advisors />
         <FeaturedSection />
         <AboutPreview />
-        <AnimatedCircles />
         <LinksSection />
       </main>
       <Footer />
